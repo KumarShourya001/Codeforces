f02a5f44de0ab411f4343ca267365439cf5140da
Kumar Shourya <kshourya2005@gmail.com>
2026-06-22 17:37:16 +0530

    update

--- D_The_67_th_OEIS_Problem.cpp
+++ D_The_67_th_OEIS_Problem.cpp
@@ -1,25 +1,59 @@
 #include <bits/stdc++.h>
-#include <vector>
 using namespace std;
 
-vector<long long>construct(int n){
-    
+const int SIZE = 200000;
+
+void prime(vector<int>&primes){
+    primes[0]=0;
+    primes[1]=0;
+    for(int i=2;i*i<=SIZE;i++){
+        if(primes[i]==1){
+            for(int j=i*i;j<SIZE;j+=i){
+                primes[j]=0;
+            }
+        }
+    }
 }
 
-int main() {
+vector<long long>construct(int n){
+    vector<int>primes(SIZE,1);
+    prime(primes);
+
+    vector<long long>plist;
+    for(int k=2;k<SIZE;k++){
+        if(primes[k]==1){
+            plist.push_back(k);
+        }
+    }
+
+    vector<long long>ans;
+    for(int i=1;i<=n;i++){
+        if(i==1){
+            ans.push_back(plist[0]);
+        }
+        else if(i==n){
+            ans.push_back(plist[n-2]);
+        }
+        else{
+            ans.push_back(plist[i-2]*plist[i-1]);
+        }
+    }
+    return ans;
+}
+
+int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-   int t;
-    cin >> t;
-    while (t--) {
-       int n;
-        cin >> n;
-        vector<long long>ans= construct(n);
-        for(auto it:ans){
-            cout<<it<<" ";
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<long long>ans=construct(n);
+        for(size_t i=0;i<ans.size();i++){
+            cout<<ans[i]<<" \n"[i==ans.size()-1];
         }
-        cout<<endl;
     }
     return 0;
 }
