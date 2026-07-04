f54de24b36d4fdfada51ca0501ec6e1990bab1da
Kumar Shourya <kshourya2005@gmail.com>
2026-07-05 03:21:23 +0530

    UPD

---
+++ D_Odd_Queries.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        int n, q;
+        cin >> n >> q;
+        vector<long long> a(n);
+        for(int i = 0; i < n; i++){
+            cin >> a[i];
+        }
+        while(q--){
+            int l, r;
+            long long k;
+            cin >> l >> r >> k;
+            
+        }
+    }
+    return 0;
+}
