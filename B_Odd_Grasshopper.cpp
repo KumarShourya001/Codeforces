8b568d6c743739d25398ce4863ca84b140661ce0
Kumar Shourya <kshourya2005@gmail.com>
2026-08-03 16:40:23 +0530

    solved

--- B_Odd_Grasshopper.cpp
+++ B_Odd_Grasshopper.cpp
@@ -1,37 +1,48 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-long long solve(long long x,long long n){
+long long solve(long long x, long long n) {
 
-    if(n%4==0)return x;
-    long long strt=4*(n/4)+1;
-    if(x%2==0){
-        
-        x-=strt;
-        strt++;
-        x+=strt;
-        strt++;
-        x+=strt;
-    }
-    else{
-        x+=strt;
-        strt++;
-        x-=strt;
-        strt++;
-        x-=strt;
-    }
+  if (n % 4 == 0)
     return x;
-
+  long long strt = 4 * (n / 4);
+  int cyc = n - strt;
+  strt++;
+  if (x % 2 == 0) {
+    if (cyc >= 1) {
+      x -= strt;
+      strt++;
+    }
+    if (cyc >= 2) {
+      x += strt;
+      strt++;
+    }
+    if (cyc >= 3)
+      x += strt;
+  } else {
+    if (cyc >= 1) {
+      x += strt;
+      strt++;
+    }
+    if (cyc >= 2) {
+      x -= strt;
+      strt++;
+    }
+    if (cyc >= 3) {
+      x -= strt;
+    }
+  }
+  return x;
 }
 
-int main(){
-    int t;
-    cin>>t;
-    while(t--){
-        long long x;
-        long long n;
-        cin>>x>>n;
-        long long ans=solve(x, n);
-        cout<<ans<<endl;    
-    }
+int main() {
+  int t;
+  cin >> t;
+  while (t--) {
+    long long x;
+    long long n;
+    cin >> x >> n;
+    long long ans = solve(x, n);
+    cout << ans << endl;
+  }
 }
