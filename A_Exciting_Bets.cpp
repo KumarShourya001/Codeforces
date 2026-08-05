f6d75c8cd1316479bb397566522b931651b4932c
Kumar Shourya <kshourya2005@gmail.com>
2026-08-05 19:29:39 +0530

    update

--- A_Exciting_Bets.cpp
+++ A_Exciting_Bets.cpp
@@ -2,8 +2,15 @@
 #include <vector>
 using namespace std;
 
-long long solve(long long a,long long b){
-    
+vector<long long> solve(long long a,long long b){
+    if(abs(a-b)==1)return  {1,0};
+    if(a==0 || b==0)return {a==0?b:a,0};
+    if(a==b)return {0,0};
+    long long g=abs(a-b);
+    long long up=(g-(a%g))%g;
+    long long down=a%g;
+    long long val=min(up,down);
+    return {g,val};
 }
 
 int main(){
@@ -12,7 +19,7 @@ int main(){
     while(t--){
         long long a,b;
         cin>>a>>b;
-        long long ans=solve(a, b);
-        cout<<ans<<endl;
+        vector<long long> ans=solve(a, b);
+        cout<<ans[0]<<" "<<ans[1]<<endl;
     }
 }
