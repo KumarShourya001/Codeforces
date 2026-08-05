26c8a4baf4e7abd59079bbb3afd46a59b1675bed
Kumar Shourya <kshourya2005@gmail.com>
2026-08-05 19:02:39 +0530

    new

---
+++ A_Exciting_Bets.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#include <vector>
+using namespace std;
+
+long long solve(long long a,long long b){
+    
+}
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        long long a,b;
+        cin>>a>>b;
+        long long ans=solve(a, b);
+        cout<<ans<<endl;
+    }
+}
