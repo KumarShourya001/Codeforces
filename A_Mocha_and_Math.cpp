c1279e21553a90d40ff3aa40b79575b84ea28e44
Kumar Shourya <kshourya2005@gmail.com>
2026-08-05 18:56:49 +0530

    solved:

---
+++ A_Mocha_and_Math.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#include <vector>
+using namespace std;
+
+int solve(int n,vector<int>&a){
+    int idx=0;
+    int mx=INT_MIN;
+    int mn=INT_MAX;
+    int ans=a[0];
+    for(int i=0;i<n;i++){
+        if(a[i]>mx){
+            mx=a[i];
+            idx=i;
+        }
+        mn=min(mn,a[i]);
+        ans=ans&a[i];
+    }
+    return ans;
+
+}
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int>a(n);
+        for(int i=0;i<n;i++)cin>>a[i];
+        int ans=solve(n,a);
+        cout<<ans<<"\n";
+
+    }
+}
