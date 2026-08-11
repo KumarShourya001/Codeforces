449c87e70bd591010aa5913d46551552f00f0422
Kumar Shourya <kshourya2005@gmail.com>
2026-08-11 22:12:18 +0530

    new:

--- B_Swap_and_Delete.cpp
+++ B_Swap_and_Delete.cpp
@@ -4,10 +4,18 @@ using namespace std;
 
 int solve(string &s) {
     int n=s.size();
+    queue<int>q;
     int cnt0=count(s.begin(),s.end(),'0');
     int cnt1=count(s.begin(),s.end(),'1');
     if(cnt1==cnt0)return 0;
-    //find cont 1 and cont 0;from start;
+    for(int i=0;i<n;i++){
+        if(q.empty())q.push(i);
+        else if(s[q.front()]==s[i])q.push(i);
+        else q.pop();
+    }
+    if(q.empty())return 0;
+    return n- q.front();
+    
 }
 
 int main() {
